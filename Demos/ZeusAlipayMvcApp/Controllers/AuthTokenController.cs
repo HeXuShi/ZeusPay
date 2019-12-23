a0ef8daed0dd0ba220925060558ef9eac71b80e8
赫敏璋 <loongsking@hotmail.com>
2019-12-23 21:34:34 +0800

    add alipay  jump to get token

---
+++ Demos/ZeusAlipayMvcApp/Controllers/AuthTokenController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ZeusAlipayMvcApp.Controllers
+{
+    public class AuthTokenController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index([FromForm]string AppId)
+        {
+            string redirectUri = HttpContext.Request.Scheme + "://" + HttpContext.Request.Host + "/GetToken";
+            string RequestPath = $"https://openauth.alipay.com/oauth2/appToAppAuth.htm?app_id={AppId}&redirect_uri=" + redirectUri;
+            return Redirect(RequestPath);
+        }
+        [HttpGet("GetToken")]
+        public IActionResult GetToken(string app_id, string app_auth_code)
+        {
+            return Json("AppId =" + app_id + ",app_auth_code="  + app_auth_code);
+        }
+    }
+}
